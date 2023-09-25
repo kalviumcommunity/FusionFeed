74617ef07aecba634381e887b642e80369d7c102
Balaji S <balajisr000@gmail.com>
2023-09-25 21:19:01 +0530

    clean coded

--- main.py
+++ main.py
@@ -3,24 +3,45 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
-MONGO = os.getenv('MONGO')
-
-client = MongoClient(MONGO)
-
-db = client.test
-collection = db["OOPs"]
-
 
 app = Flask(__name__)
 
+# Configuration
+MONGO = os.getenv('MONGO')
+client = MongoClient(MONGO)
+db = client.test
+collection = db["OOPs"]
+
+class Document:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+        print(f"Document object created for {self.name}")
+
+
+    def __del__(self):
+        print(f"Document object destroyed for {self.name}")
+
 @app.route('/add', methods=['POST'])
-def create_database():
+def create_document():
     try:
-        new_doc = {"name": "A", "age": 18}
-        result = collection.insert_one(new_doc)
-        return str(result.inserted_id), 200
+        data = request.get_json()
+        name = data.get('name')
+        email = data.get('email')
+        password = data.get('password')
+
+        if not (name and email and password):
+            return jsonify({'error': 'Missing data'}), 400
+
+        # Create an instance of the Document class
+        new_doc = Document(name, email, password)
+        doc_data = {"name": new_doc.name, "email": new_doc.email, "password": new_doc.password}
+        
+        # Insert the document into the collection
+        result = collection.insert_one(doc_data)
+        return jsonify({'message': 'Document created', 'document_id': str(result.inserted_id)}), 201
     
     except Exception as e:
         return jsonify({'error': str(e)}), 500
