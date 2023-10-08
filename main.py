95b901eecb24565c4b57248118fb434e941953dd
BalajiSriraman <balajisr000@gmail.com>
2023-10-08 13:12:08 +0530

    chore: Branch reset

--- main.py
+++ main.py
@@ -14,25 +14,18 @@ db = client.test
 collection = db["OOPs"]
 
 
-# Classes and Objects
 class Document:
-
-    # Constructor
     def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
         print(f"Document object created for {self.name}")
 
-    # Destructor
     def __del__(self):
         print(f"Document object destroyed for {self.name}")
 
 
-"""This Route is for creating a new user"""
-
-
-@app.route('/signup', methods=['POST'])
+@app.route('/add', methods=['POST'])
 def create_document():
     try:
         data = request.get_json()
@@ -56,66 +49,5 @@ def create_document():
         return jsonify({'error': str(e)}), 500
 
 
-"""This Route is for creating a new blog post"""
-
-
-@app.route('/blog-post', methods=['POST'])
-def create_post():
-
-    try:
-        data = request.get_json()
-        email = request.headers.get('email')
-        password = request.headers.get('password')
-
-        if not (email and password):
-            return jsonify({'error': 'Missing email or password headers'}), 400
-
-        # Check if the email and password match with the ones stored in the database
-        doc_data = collection.find_one({"email": email, "password": password})
-        if not doc_data:
-            return jsonify({'error': 'Invalid email or password'}), 401
-
-        # Create a new post
-        # ...
-
-        return jsonify({'message': 'Post created'}), 201
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
-"""This Route is for getting all blog posts"""
-
-
-@app.route('/posts', methods=['GET'])
-def get_posts():
-    pass
-
-
-"""This Route is for getting a single blog post by id"""
-
-
-@app.route('/post/<id>', methods=['GET'])
-def get_post(id):
-    pass
-
-
-"""This Route is for updating a single blog post by id"""
-
-
-@app.route('/post/<id>', methods=['PUT'])
-def update_post(id):
-    pass
-
-
-"""This Route is for deleting a single blog post by id"""
-
-
-@app.route('/post/<id>', methods=['DELETE'])
-def delete_post(id):
-    pass
-
-
-# debug
 if __name__ == '__main__':
     app.run(debug=True)
