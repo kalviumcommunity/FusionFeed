502ee256f56f43f8c1cba67c470baf8d8a600d40
BalajiSriraman <balajisr000@gmail.com>
2023-10-08 16:39:42 +0530

    auth using staticmethod

---
+++ auth.py
@@ -0,0 +1,17 @@
+import os
+from pymongo import MongoClient
+
+MONGO = os.getenv('MONGO')
+Auth = MongoClient(MONGO).test["OOPs"]
+
+
+class AuthValidation:
+    @staticmethod
+    def Validate(email, password):
+        print(f"Validating {email} and {password}")
+        # Check if the email and password match with the ones stored in the database
+        doc_data = Auth.find_one({"email": email, "password": password})
+        if doc_data:
+            return doc_data["name"]
+        else:
+            return None
