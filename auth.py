8930e8bc36dfa77df2f015638f663f72bc3b9881
BalajiSriraman <balajisr000@gmail.com>
2023-10-19 11:54:14 +0530

    wip

--- auth.py
+++ auth.py
@@ -15,28 +15,43 @@ class User(ABC):
         self._email = email
         self._password = password
 
-    @abstractmethod
-    def get_user_name(self):
-        pass
+    def get_id(self):
+        return self._id
+
+    def get_name(self):
+        return self._name
+
+    def get_email(self):
+        return self._email
+
+    def get_password(self):
+        return self._password
 
 
-class UserAuthenticator(User):
+class UserAuthenticator:
     @staticmethod
     def find_user(email, password):
         doc_data = Auth.find_one({"email": email, "password": password})
         if doc_data:
-            user = UserAuthenticator(doc_data['_id'], doc_data['name'],
-                                     doc_data['email'], doc_data['password'])
+            user = User(doc_data['_id'], doc_data['name'],
+                        doc_data['email'], doc_data['password'])
             return user
         else:
-            return False
+            return None
 
-    def get_user_name(self):
-        return self._name
+
+class UserNameGetter(ABC):
+    @staticmethod
+    @abstractmethod
+    def get_user_name(user):
+        if user:
+            return user.get_name()
+        else:
+            return None
 
 
 class AuthValidation:
     @staticmethod
-    def Validate(email, password):
+    def validate(email, password):
         user = UserAuthenticator.find_user(email, password)
-        return user.get_user_name() if user else False
+        return UserNameGetter.get_user_name(user)
