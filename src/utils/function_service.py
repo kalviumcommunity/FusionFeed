c28f36122d87ab211ca7df0bafa0cadb0459cf6b
Balaji S <balajisr000@gmail.com>
2023-11-20 12:34:36 +0530

    Route for getting a single blog post by id

--- src/utils/function_service.py
+++ src/utils/function_service.py
@@ -1,5 +1,23 @@
+from flask import jsonify
+
+
+blog_posts = []  
+
+
 # def get_posts():
 #     try:
 #         return jsonify({'posts': blog_posts}), 200
 #     except Exception as e:
 #         return jsonify({'error': str(e)}), 500
+
+
+def get_post(id):
+    try:
+        # Find the post with the given ID
+        post = next((post for post in blog_posts if post['id'] == id), None)
+        if post:
+            return jsonify({'post': post}), 200
+        else:
+            return jsonify({'error': 'Post not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
