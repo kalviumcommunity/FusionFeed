e04386efec726b116065b91ae3c13b3b48ad2590
Balaji S <balajisr000@gmail.com>
2023-11-20 12:48:27 +0530

    chore: Move Blog Functions

--- src/routes/user_functions.py
+++ src/routes/user_functions.py
@@ -1,7 +1,6 @@
-from flask import request, Response, jsonify
+from flask import request, jsonify
 from src.models.doc_model import DocumentHandler
-from src.middlewares.auth_mid import AuthValidation
-from src.models.doc_model import DocumentHandler, BlogPostHandler
+from src.models.doc_model import DocumentHandler
 
 from pymongo import MongoClient
 import os
@@ -24,41 +23,6 @@ def create_document():
 
         result = Auth.insert_one(doc_data)
         return jsonify({'message': 'Document created', 'document_id': str(result.inserted_id)}), 201
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
-def create_post():
-    
-    blog_posts = []  # A list to store blog posts
-
-    try:
-        data = request.get_json()
-        email = request.headers.get('email')
-        password = request.headers.get('password')
-
-        if not (email and password):
-            return jsonify({'error': 'Missing email or password headers'}), 400
-
-        doc_data = AuthValidation.Validate(email, password)
-
-        if not doc_data:
-            return jsonify({'error': 'Invalid email or password'}), 401
-
-        title = data.get('title')
-        content = data.get('content')
-        author = doc_data
-
-        if not (title and content):
-            return jsonify({'error': 'Missing title or content'}), 400
-
-        post_handler = BlogPostHandler()
-        post_data = post_handler.create_data(title, content, author)
-
-        blog_posts.append(post_data)
-
-        return jsonify({'message': 'Blog post created', 'post': post_data}), 201
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
--- src/utils/blog_functions.py
+++ src/utils/blog_functions.py
@@ -1,15 +1,51 @@
 from flask import jsonify, request
+from src.models.doc_model import BlogPostHandler
+from src.middlewares.auth_mid import AuthValidation
 
 
 blog_posts = []  
 
 
-# def get_posts():
-#     try:
-#         return jsonify({'posts': blog_posts}), 200
-#     except Exception as e:
-#         return jsonify({'error': str(e)}), 500
 
+def create_post():
+    
+    blog_posts = []  # A list to store blog posts
+
+    try:
+        data = request.get_json()
+        email = request.headers.get('email')
+        password = request.headers.get('password')
+
+        if not (email and password):
+            return jsonify({'error': 'Missing email or password headers'}), 400
+
+        doc_data = AuthValidation.Validate(email, password)
+
+        if not doc_data:
+            return jsonify({'error': 'Invalid email or password'}), 401
+
+        title = data.get('title')
+        content = data.get('content')
+        author = doc_data
+
+        if not (title and content):
+            return jsonify({'error': 'Missing title or content'}), 400
+
+        post_handler = BlogPostHandler()
+        post_data = post_handler.create_data(title, content, author)
+
+        blog_posts.append(post_data)
+
+        return jsonify({'message': 'Blog post created', 'post': post_data}), 201
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+def get_posts():
+    try:
+        return jsonify({'posts': blog_posts}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 def get_post(id):
     try:
