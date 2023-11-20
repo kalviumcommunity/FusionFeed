33a9b3d1d8a5b8478cccf55246c00b4f089f87e1
Balaji S <balajisr000@gmail.com>
2023-11-20 07:57:06 +0000

    make all routes functional with DB

--- src/utils/blog_functions.py
+++ src/utils/blog_functions.py
@@ -2,15 +2,16 @@ from flask import jsonify, request
 from src.models.doc_model import BlogPostHandler
 from src.middlewares.auth_mid import AuthValidation
 
+from pymongo import MongoClient
+from bson import ObjectId  # Import ObjectId from bson
+import os
 
-blog_posts = []  
-
-
+MONGO_URI = os.getenv('MONGO')
+mongo_client = MongoClient(MONGO_URI)
+db = mongo_client.test
+posts_collection = db["blog_posts"]
 
 def create_post():
-    
-    blog_posts = []  # A list to store blog posts
-
     try:
         data = request.get_json()
         email = request.headers.get('email')
@@ -34,24 +35,35 @@ def create_post():
         post_handler = BlogPostHandler()
         post_data = post_handler.create_data(title, content, author)
 
-        blog_posts.append(post_data)
+        # Insert the post into MongoDB
+        result = posts_collection.insert_one(post_data)
 
-        return jsonify({'message': 'Blog post created', 'post': post_data}), 201
+        return jsonify({'message': 'Blog post created', 'post_id': str(result.inserted_id)}), 201
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 def get_posts():
     try:
-        return jsonify({'posts': blog_posts}), 200
+        # Retrieve all posts from MongoDB
+        posts = list(posts_collection.find())
+
+        # Convert ObjectId to string for JSON serialization
+        for post in posts:
+            post['_id'] = str(post['_id'])
+
+        return jsonify({'posts': posts}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 def get_post(id):
     try:
-        # Find the post with the given ID
-        post = next((post for post in blog_posts if post['id'] == id), None)
+        # Find the post with the given ID in MongoDB
+        post = posts_collection.find_one({'_id': ObjectId(id)})
+
         if post:
+            # Convert ObjectId to string for JSON serialization
+            post['_id'] = str(post['_id'])
             return jsonify({'post': post}), 200
         else:
             return jsonify({'error': 'Post not found'}), 404
@@ -64,29 +76,33 @@ def update_post(id):
         title = data.get('title')
         content = data.get('content')
 
-        # Find the post with the given ID
-        post = next((post for post in blog_posts if post['id'] == id), None)
+        # Find the post with the given ID in MongoDB
+        post = posts_collection.find_one({'_id': ObjectId(id)})
 
         if not post:
             return jsonify({'error': 'Post not found'}), 404
 
-        # Update the post with the new data
-        post['title'] = title
-        post['content'] = content
-        return jsonify({'message': 'Blog post updated', 'post': post}), 200
+        # Update the post with the new data in MongoDB
+        posts_collection.update_one({'_id': ObjectId(id)}, {'$set': {'title': title, 'content': content}})
+
+        # Return the updated post
+        updated_post = posts_collection.find_one({'_id': ObjectId(id)})
+
+        # Convert ObjectId to string for JSON serialization
+        updated_post['_id'] = str(updated_post['_id'])
+
+        return jsonify({'message': 'Blog post updated', 'post': updated_post}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 def delete_post(id):
     try:
-        # Find the index of the post with the given ID
-        index = next((i for i, post in enumerate(
-            blog_posts) if post['id'] == id), None)
+        # Delete the post with the given ID from MongoDB
+        result = posts_collection.delete_one({'_id': ObjectId(id)})
 
-        if index is not None:
-            deleted_post = blog_posts.pop(index)
-            return jsonify({'message': 'Blog post deleted', 'post': deleted_post}), 200
+        if result.deleted_count > 0:
+            return jsonify({'message': 'Blog post deleted'}), 200
         else:
             return jsonify({'error': 'Post not found'}), 404
 
