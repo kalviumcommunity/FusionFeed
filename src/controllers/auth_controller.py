a1a0990a63c2ecf3ba2c7d643ff4c6495f0373c6
Balaji S <balajisr000@gmail.com>
2023-11-20 12:48:00 +0530

    feat: new Routes

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -7,7 +7,8 @@ MONGO = os.getenv('MONGO')
 Auth = MongoClient(MONGO).test["OOPs"]
 
 # Route Functions
-from src.routes.user_functions import create_document , create_post
+from src.routes.user_functions import create_document
+from src.utils.blog_functions import create_post, get_post , update_post , delete_post , get_posts
 
 # user controller blueprint to be registered with api blueprint
 app = Blueprint("users", __name__)
@@ -15,3 +16,13 @@ app = Blueprint("users", __name__)
 app.route('/signup', methods=['POST'])(create_document)
 
 app.route('/blog-post', methods=['POST'])(create_post)
+
+app.route('/posts', methods=['GET'])(get_posts)
+
+app.route('/post/<id>', methods=['GET'])(get_post)
+
+app.route('/post/<id>', methods=['PUT'])(update_post)
+
+app.route('/post/<id>', methods=['DELETE'])(delete_post)
+
+
