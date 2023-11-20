bb1954eae91dedc96b3791e349b49c515b30c084
Balaji S <balajisr000@gmail.com>
2023-11-20 12:35:34 +0530

    Route for updating a single blog post by id

--- src/utils/function_service.py
+++ src/utils/function_service.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 
 
 blog_posts = []  
@@ -19,5 +19,25 @@ def get_post(id):
             return jsonify({'post': post}), 200
         else:
             return jsonify({'error': 'Post not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+def update_post(id):
+    try:
+        data = request.get_json()
+        title = data.get('title')
+        content = data.get('content')
+
+        # Find the post with the given ID
+        post = next((post for post in blog_posts if post['id'] == id), None)
+
+        if not post:
+            return jsonify({'error': 'Post not found'}), 404
+
+        # Update the post with the new data
+        post['title'] = title
+        post['content'] = content
+        return jsonify({'message': 'Blog post updated', 'post': post}), 200
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
