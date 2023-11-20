36d3c240d3c70e5b4889471e2a64d7d0b38a43e0
Balaji S <balajisr000@gmail.com>
2023-11-20 12:25:07 +0530

    App Setup

---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+from src.config.config import Config
+from dotenv import load_dotenv
+from src.routes.index import api
+
+# loading environment variables
+load_dotenv()
+
+# declaring flask application
+app = Flask(__name__)
+
+# calling the dev configuration
+config = Config().dev_config
+
+# making our application to use dev env
+app.env = config.ENV
+
+app.register_blueprint(api)
